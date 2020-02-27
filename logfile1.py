e0b057c7eea79411544dd9c8a8ba3ece003f42f4
Krishnamurthi <ajay.krishnamurthi@accenture.com>
2020-02-27 11:54:41 +0530

    seocnd try

---
+++ logfile1.py
@@ -0,0 +1,75 @@
+import logging
+import mysql.connector
+
+# Database Connectivity
+mydb = mysql.connector.connect(host = "localhost",user = "root",passwd = "root",db = "assessment4")
+
+logging.basicConfig(filename="logf.txt",
+                            filemode='a',
+                            format='%(asctime)s - %(message)s',
+                            level=logging.INFO)
+
+# logging.info("Database connected")
+# class Database:
+#     def database_operation(self):
+#         cursor = mydb.cursor()
+#
+#         # To find a Maximum - sold product with product details fetched
+#
+#         cursor.execute("select product_name, product_code, price from (select product_name, product_code, price, dense_rank() over(order by product_quantity desc) as r from product p join sale s on p.id = s.id) as t where r = 1")
+#         logging.info("Maximum - sold product with product details fetched")
+#         max_sold = cursor.fetchall()
+#         print("Maximum - sold product with product details")
+#         print(max_sold)
+#
+#         # To find a Minimum - sold product with product details fetched
+#
+#         cursor.execute("select product_name, product_code, price from (select product_name, product_code, price, dense_rank() over(order by product_quantity) as r from product p join sale s on p.id = s.id) as t where r = 1")
+#         logging.info("Minimum - sold product with product details fetched")
+#         min_sold = cursor.fetchall()
+#         print("Minimum - sold product with product details")
+#         print(min_sold)
+#
+#         # To Get the billing date from user and print the product details and total amount of displaying products
+#
+#         date_inp = input("Enter a date : ")
+#         cursor.execute("select product_name, product_code, price*product_quantity as total_amount from product p join sale s on p.id = s.id where bill_date = %s",(date_inp,))
+#         logging.info("Billing Date: ")
+#         bill_dates = cursor.fetchall()
+#         print("Billing Date ")
+#         print(bill_dates)
+#
+#         # To Get the product code from user and check it in product table
+#
+#         prod_code = input("Enter a product code : ")
+#         cursor.execute("select product_name, product_code, price from product where product_code = %s",(prod_code,))
+#         logging.info("Product code ")
+#         product_codes = cursor.fetchall()
+#         print("Product code: ")
+#         print(product_codes)
+#
+#         # Add some products to product table
+#
+#         cursor.execute("insert into product values (4,'Lenovo', 'Idepad S145', 44500)")
+#         logging.info("Successfully inserted")
+#         print("Successfully inserted ")
+#         mydb.commit()
+#
+#         mydb.close()
+#
+# data = Database()
+# data.database_operation()
+#
+# #*******************************O/P*********************************************
+#
+# # Maximum - sold product with product details
+# # [('YAMAHA Speakers', 'YHT-1840', 26500)]
+# # Minimum - sold product with product details
+# # [('JBL Speakers', 'JBL-100', 18600)]
+# # Enter a date : 2020-01-01
+# # Billing Date
+# # [('YAMAHA Speakers', 'YHT-1840', 106000)]
+# # Enter a product code : SONY-LED-S43
+# # Product code:
+# # [('SONY LED TV', 'SONY-LED-S43', 58300)]
+# # Successfully inserted
